943420520c01981d1e8a7721121af2e28d604be1
gandori <marcel.stiebing@web.de>
2022-05-21 08:33:45 +0200

    complete overhaul

--- src/classes/method_post.py
+++
@@ -1,9 +0,0 @@
-from flask import redirect ,url_for
-
-class method_post:
-    def __init__(self) -> None:
-        
-        def post():
-            pass
-        
-        self.post = post()
--- src/main.py
+++ src/main.py
@@ -1,31 +1,41 @@
-from flask import Flask ,redirect ,url_for ,request
-
-from classes.method_post import method_post
-from classes.method_get import method_get
+from flask import Flask, redirect, url_for, request, render_template
+import os
 
 class app:
-    def __init__(self) -> None:
-        self = Flask(__name__)
+	def __init__(self):
+		self = Flask(__name__)
+		_dir = os.path.dirname(os.path.abspath(__file__))
+		self.template_folder = os.path.join(_dir, "templates")
+		self.static_folder = os.path.join(_dir, "static")
+		self.debug = True
 
-        @self.route("/<page>" ,methods = ["GET"])
-        def other(page):
-            return redirect(url_for("index"))
+		@self.errorhandler(404)
+		def page_not_found(e):
+			return redirect(url_for("index"))
 
-        @self.route("/" ,methods = ["GET"])
-        def slash():
-            
-            return redirect(url_for("index"))
+		@self.before_request
+		def before_request():
+			pass
 
-        @self.route("/index.html" ,methods = ["GET" ,"POST"])
-        def index():
+		@self.after_request
+		def after_request(response):
+			response.headers.add("Access-Control-Allow-Origin", "*")
+			response.headers.add("Cache-control", "no-cache, no-store, must-revalidate")
+			return response
 
-            if request.method == "POST": 
-                return method_post().post
+		@self.route("/<page>", methods = ["GET"])
+		def other(page):
+			return redirect(url_for("index"))
 
-            if request.method == "GET":
-                return method_get().get
+		@self.route("/", methods = ["GET"])
+		def slash():
+			return redirect(url_for("index"))
 
-        self.run(threaded = True ,debug=True)
+		@self.route("/index.html", methods = ["GET"])
+		def index():
+			return render_template("index.html")
+
+		self.run(host="localhost", port=1000, threaded=True)
 
 if __name__ == "__main__":
-    app()
+	app()
